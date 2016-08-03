ab0a439439473a8343737e551a694dfb59b26ea6
dferry <dferry@slu.edu>
2016-08-03 13:39:29 -0500

    added hello_printf.c files

---
+++ studios/01_hello_world/hello_printf.c
@@ -0,0 +1,12 @@
+//Hello world program
+//Prints hello world to console with fprintf
+//David Ferry
+//August 3, 2016
+
+int main( int argc, char* argv[] ){
+
+
+	fprintf(stdout, "Hello world!\n");
+
+	return 0;
+}
